4364532edebdeedfc974b3e82a1012b2529595f5
txperl <txperl@gmail.com>
2016-09-15 22:12:33 +0800

    1.0 beta1
    非正式开源版

---
+++ d/index.php
@@ -0,0 +1,271 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="UTF-8">
+    <title>InfoDownload -airAnime Online</title>
+    <link rel='stylesheet prefetch' href='css/jquery-ui1.11.2.css'>
+<style>
+* { -moz-box-sizing: border-box; -webkit-box-sizing: border-box; box-sizing: border-box; }
+
+.list-wrap label {
+  float:left;
+  color:#00BDE8;
+}
+.search-box {
+  float:left;
+  clear:left;
+  width:70%;
+  padding:0.4em;
+  font-size:1em;
+  color:#555;
+}
+
+.list-count {
+  float:left;
+  text-align:center;
+  width:30%;
+  padding:0.5em;
+  color:#ddd;
+}
+
+li {
+  transition-property: margin, background-color, border-color;
+  transition-duration: .4s, .2s, .2s;
+  transition-timing-function: ease-in-out, ease, ease;
+  font-size: 14px;
+  font-family: 'Microsoft YaHei', Tahoma, Helvetica, Arial;
+}
+
+.empty-item {
+  transition-property: opacity;
+  transition-duration: 0s;
+  transition-delay: 0s;
+  transition-timing-function: ease;
+}
+
+.empty .empty-item {
+  transition-property: opacity;
+  transition-duration: .2s;
+  transition-delay: .3s;
+  transition-timing-function: ease;
+}
+
+.hiding {
+  margin-left:-100%;
+  opacity:0.5;
+}
+
+.hidden {
+  display:none;
+}
+
+ul {
+  float:left;
+  width:100%;
+  margin:2em 0;
+  padding:0;
+  position:relative;
+}
+
+ul:before {
+  content:'results';
+  position:absolute;
+  left:-2.8em;
+  font-size:3em;
+  text-align:right;
+  top:1.5em;
+  color:#ededed;
+  font-weight:bold;
+  font-family: 'Maven Pro', sans-serif;
+  transform:rotate(-90deg);
+}
+
+li {
+  float:left;
+  clear:left;
+  width:100%;
+  margin:0.2em 0;
+  padding:0.5em 0.8em;
+  list-style:none;
+  background-color:#f2f2f2;
+  border-left:5px solid #003842;
+  cursor:pointer;
+  color:#333;
+  position:relative;
+  z-index:2;
+}
+
+li:hover {
+  background-color:#f9f9f9;
+  border-color:#00BDE8;
+}
+
+.empty-item {
+  background:#fff;
+  color:#ddd;
+  margin:0.2em 0;
+  padding:0.5em 0.8em;
+  font-style:italic;
+  border:none;
+  text-align:center;
+  visibility:hidden;
+  opacity:0;
+  float:left;
+  clear:left;
+  width:100%;
+}
+
+.empty .empty-item {
+  opacity:1;
+  visibility:visible;
+}
+
+body {
+  background-color:#fff;
+  font-family:'Open Sans', sans-serif;
+  margin:0;
+  padding:0;
+  font-size:1em;
+}
+
+a {color:#00BDE8;}
+
+h1 {
+  font-size:2.6em;
+  margin:0;
+  padding-top:1.5em;
+  text-align:center;
+  font-family: 'Maven Pro', sans-serif;
+}
+h3 {
+  margin:0 0 2em;
+  text-align:center;
+font-weight:normal;
+font-family: georgia, times;
+font-style:italic;
+  color:#777;
+  font-size:1em;
+}
+
+.info {
+  float:left;
+  width:60%;
+  margin:2em 20%;
+  padding:2em 0;
+  background:#f9f9f9;
+  border-left:5px solid #003842;
+  padding:10px 20px;
+}
+
+.list-wrap {
+  float:left;
+  width:65%;
+  margin:2em 20%;
+  padding:2em 0;
+}
+
+p {
+  text-align:left;
+  font-size:1em;
+}
+
+.cta {
+  float:left;
+  width:100%;
+  text-align:center;
+  color:#999;
+  font-family:georgia, times;
+  font-style:italic;
+  margin:2em 0;
+}
+
+.cta a {
+  font-size:1.5em;
+  font-style:normal;
+  font-family: 'Maven Pro', sans-serif;
+  text-decoration:none;
+  line-height:1.5em;
+}
+
+.topdeco {
+  float:left;
+  width:100%;
+  height:10px;
+  position:fixed;
+  z-index:10;
+}
+
+.topdeco span {
+  float:left;
+  width:25%;
+  height:100%;
+}
+
+.deco span:nth-child(1) {
+  background:#FF8220;
+}
+.deco span:nth-child(2) {
+  background:#000;
+}
+.deco span:nth-child(3) {
+  background:#FFA00A;
+}
+.deco span:nth-child(4) {
+  background:#00BDE8;
+}
+
+.ddbb{
+	display: block;
+    float: right;
+    font-family: 'Microsoft YaHei', Tahoma, Helvetica, Arial;
+    font-size: 10px;
+}
+</style> 
+        <script src="js/prefixfree.min.js"></script>
+  </head>
+  <body>
+
+<div class="deco topdeco">
+  <span></span>
+  <span></span>
+  <span></span>
+  <span></span>
+  </div>
+
+<?php
+  $title = $_SERVER['QUERY_STRING'];
+    echo '<h1>'.urldecode($title).'</h1>';
+?>
+
+ <section class="list-wrap">
+
+   <label for="search-text">Search the list</label>
+  <input type="text" id="search-text" placeholder="search" class="search-box">
+    <span class="list-count"></span>
+    
+    
+<ul id="list">
+<?php
+require "../mains.php";
+  $title = $_SERVER['QUERY_STRING'];
+  $rst=DMHY($title);
+  for ($i=0; $i < $rst[5]; $i++) { 
+    echo '<a target="_blank" href="'.$rst[1][$i+1].'"><li class="in">'.$rst[0][$i+1].'<div class="ddbb">'.$rst[2][$i+1].' | '.$rst[4][$i+1].'</div></li></a>';
+  }
+  //<a href="{dmhy_地址}"><li class="in">{dmhy_标题}<div class="ddbb">{dmhy_info}</div></li></a>
+?>
+  <span class="empty-item">no results</span>
+</ul>
+   </section>
+   
+<p class="cta">
+  <?php
+  $title = $_SERVER['QUERY_STRING'];
+    echo 'Page from robsawyer.me<br /> <a id="link" target="_blank" href="http://share.dmhy.org/topics/list?keyword='.$title.'">Detailed info.</a>'
+    ?>
+</p>
+<script src='js/jquery.min2.1.3.js'></script>
+<script src='js/jquery-ui.min1.11.2.js'></script>
+<script src="js/index.js"></script>
+</body>
+</html><script> throw new Error(""); //SAE未实名特殊处理
---
+++ function.php
@@ -0,0 +1,96 @@
+<?php
+
+/*以下是取中间文本的函数 
+  getSubstr=调用名称
+  $str=预取全文本 
+  $leftStr=左边文本
+  $rightStr=右边文本
+*/
+function getSubstr($str, $leftStr, $rightStr)
+{
+    $left = strpos($str, $leftStr);
+    //echo '左边:'.$left;
+    $right = strpos($str, $rightStr,$left);
+    //echo '<br>右边:'.$right;
+    if($left < 0 or $right < $left) return '';
+    return substr($str, $left + strlen($leftStr), $right-$left-strlen($leftStr));
+}
+function getHTTPS($url) {
+  $ch = curl_init();
+  curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, FALSE);
+  curl_setopt($ch, CURLOPT_HEADER, false);
+  curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
+  curl_setopt($ch, CURLOPT_URL, $url);
+  curl_setopt($ch, CURLOPT_REFERER, $url);
+  curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
+  $result = curl_exec($ch);
+  curl_close($ch);
+  return $result;
+}
+function curl_get_contents($url)   
+{   
+    $ch = curl_init();   
+    curl_setopt($ch, CURLOPT_URL, $url);            //设置访问的url地址   
+    //curl_setopt($ch,CURLOPT_HEADER,1);            //是否显示头部信息   
+    curl_setopt($ch, CURLOPT_TIMEOUT, 10);           //设置超时   
+    //curl_setopt($ch, CURLOPT_USERAGENT, _USERAGENT_);   //用户访问代理 User-Agent   
+    //curl_setopt($ch, CURLOPT_REFERER,_REFERER_);        //设置 referer   
+    curl_setopt($ch,CURLOPT_FOLLOWLOCATION,1);      //跟踪301   
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);        //返回结果   
+    $r = curl_exec($ch);   
+    curl_close($ch);   
+    return $r;   
+}
+//多线程抓取网页
+function curl_multi($urls) {  
+    if (!is_array($urls) or count($urls) == 0) {  
+        return false;  
+    }   
+    $num=count($urls);  
+    $curl = $curl2 = $text = array();  
+    $handle = curl_multi_init();  
+    function createCh($url) {  
+        $ch = curl_init();  
+        curl_setopt ($ch, CURLOPT_URL, $url);  
+        curl_setopt ($ch, CURLOPT_USERAGENT, 'Mozilla/5.0 (Windows NT 6.1; Trident/7.0; rv:11.0) like Gecko');//设置头部  
+        curl_setopt ($ch, CURLOPT_REFERER, $url); //设置来源  
+        curl_setopt ($ch, CURLOPT_ENCODING, "gzip"); // 编码压缩  
+        curl_setopt ($ch, CURLOPT_RETURNTRANSFER, 1);  
+        curl_setopt ($ch, CURLOPT_FOLLOWLOCATION, 1);//是否采集301、302之后的页面  
+        curl_setopt ($ch, CURLOPT_MAXREDIRS, 5);//查找次数，防止查找太深  
+        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, FALSE); // 对认证证书来源的检查  
+        curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, FALSE); // 从证书中检查SSL加密算法是否存在         
+        curl_setopt ($ch, CURLOPT_TIMEOUT, 20);  
+        curl_setopt ($ch, CURLOPT_HEADER, 0);//输出头部  
+        return $ch;  
+    }  
+    foreach($urls as $k=>$v){  
+        $url=$urls[$k];  
+        $curl[$k] = createCh($url);  
+        curl_multi_add_handle ($handle,$curl[$k]);  
+    }  
+    $active = null;  
+    do {  
+        $mrc = curl_multi_exec($handle, $active);  
+    } while ($mrc == CURLM_CALL_MULTI_PERFORM);  
+  
+    while ($active && $mrc == CURLM_OK) {  
+        if (curl_multi_select($handle) != -1) {  
+            usleep(100);  
+        }  
+        do {  
+            $mrc = curl_multi_exec($handle, $active);  
+        } while ($mrc == CURLM_CALL_MULTI_PERFORM);  
+    }   
+  
+    foreach ($curl as $k => $v) {  
+        if (curl_error($curl[$k]) == "") {  
+            $text[$k] = (string) curl_multi_getcontent($curl[$k]);   
+        }  
+        curl_multi_remove_handle($handle, $curl[$k]);  
+        curl_close($curl[$k]);  
+    }   
+    curl_multi_close($handle);  
+    return $text;  
+}
+?>
