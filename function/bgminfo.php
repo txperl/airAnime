a3a43eddef4f7ba6c7bdb2c4e5484049cd7b2616
sxyazi <sxyazi@gmail.com>
2021-12-09 18:38:10 +0800

    fix: use curl instead of file_get_contents

--- function/bgminfo.php
+++ function/bgminfo.php
@@ -1,17 +1,19 @@
 <?php
+require_once 'functions.php';
+
 $type = @$_POST['type'];
 $kt = @$_POST['keytitle'];
 
 if ($type && $kt) {
     $api_search = 'https://api.tls.moe/?app=bangumi&key=search&kt=' . $kt . '&type=' . $type;
-    $data_s = file_get_contents($api_search);
+    $data_s = curl_get_contents($api_search);
     $data_s = json_decode($data_s, true);
 
     $sid = $data_s['list'][0]['id'];
 
     if ($sid) {
         $api_info = 'https://api.tls.moe/?app=bangumi&key=info&sid=' . $sid . '&type=' . $type;
-        $data_i = file_get_contents($api_info);
+        $data_i = curl_get_contents($api_info);
         $data_i = str_replace('http:\/\/lain.bgm.tv', 'https:\/\/lain.bgm.tv', $data_i);
         print_r($data_i);
     } else {
--- function/userbgm.php
+++ function/userbgm.php
@@ -1,5 +1,7 @@
 <?php
+require_once 'functions.php';
 require_once 'class/search.class.php';
+
 if (@$_COOKIE["user_id"] && @$_COOKIE["user_ctime"]) {
     $userid = $_COOKIE["user_id"];
     $ctime = $_COOKIE["user_ctime"];
@@ -24,7 +26,7 @@ if (@$_COOKIE["user_id"] && @$_COOKIE["user_ctime"]) {
     $search->dbStart();
 
     if ($type == 'refresh') {
-        $data = file_get_contents('https://api.tls.moe/?app=bangumi&key=watching&name=' . $userid);
+        $data = curl_get_contents('https://api.tls.moe/?app=bangumi&key=watching&name=' . $userid);
         setcookie("user_id", $userid, time() + 24 * 3600 * 7, "/");
         setcookie("user_ctime", $ntime, time() + 24 * 3600 * 7, "/");
         $oriData = $search->__doExit_Userbgm($userid, $ntime, $data);
@@ -38,7 +40,7 @@ if (@$_COOKIE["user_id"] && @$_COOKIE["user_ctime"]) {
     if ($type == 'normal') {
         $oriData = $search->__doSearch_Userbgm($userid);
         if (count($oriData) == 0 || !$oriData[0]['data']) {
-            $data = file_get_contents('https://api.tls.moe/?app=bangumi&key=watching&name=' . $userid);
+            $data = curl_get_contents('https://api.tls.moe/?app=bangumi&key=watching&name=' . $userid);
             setcookie("user_id", $userid, time() + 24 * 3600 * 7, "/");
             setcookie("user_ctime", $ntime, time() + 24 * 3600 * 7, "/");
             $oriData = $search->__doExit_Userbgm($userid, $ntime, $data);
