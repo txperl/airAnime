63a1fe1485205693fbdb254184fd1a68e614b704
txperl <txperl@gmail.com>
2023-01-19 19:27:33 +0800

    feat(zhHans): add conversion between hans and hant
    - 新增 简体、繁体转换

--- vapp/source/online/copymanga.mjs
+++ vapp/source/online/copymanga.mjs
@@ -1,10 +1,14 @@
 import SourceOnline from "./root.mjs";
+import HanConvert from "../../utils/zhHan.mjs"
 
 export default class SourceCopyManga extends SourceOnline {
     async format(raw) {
         const r = [];
         for (const item of raw.results.list) {
-            r.push(this._genRlist(item.name, `https://copymanga.site/comic/${item.path_word}`));
+            r.push(this._genRlist(
+                HanConvert.t2s(item.name),
+                `https://copymanga.site/comic/${item.path_word}`)
+            );
         }
         return r.slice(0, 10);
     }
--- vapp/source/online/moxmoe.mjs
+++ vapp/source/online/moxmoe.mjs
@@ -1,4 +1,5 @@
 import SourceOnline from "./root.mjs";
+import HanConvert from "../../utils/zhHan.mjs"
 
 export default class SourceMoxMoe extends SourceOnline {
     async format(raw) {
@@ -7,7 +8,7 @@ export default class SourceMoxMoe extends SourceOnline {
         let m;
         while ((m = regex.exec(raw)) !== null) {
             if (m.index === regex.lastIndex) regex.lastIndex += 1;
-            r.push(this._genRlist(m[2], m[1]));
+            r.push(this._genRlist(HanConvert.t2s(m[2]), m[1]));
         }
         return r.slice(0, 10);
     }
