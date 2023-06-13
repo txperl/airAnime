d285e102e93c259ec6ba7f11f55fccea933e1ed4
txperl <txperl@gmail.com>
2023-06-13 14:34:28 +0800

    fix: koxmoe: fix matching of title and link
    - 修复 KoxMoe 标题和链接匹配问题
    - 更新 BimiBimi 数据

--- vapp/source/online/koxmoe.mjs
+++ vapp/source/online/koxmoe.mjs
@@ -4,7 +4,8 @@ import HanConvert from "../../utils/zhHan.mjs"
 export default class SourceKoxMoe extends SourceOnline {
     async format(raw) {
         const r = [];
-        const regex = /<\/div><\/a>.*?<a href='(.*?)'>(.*?)<\/a>/gms;
+        const regex =
+            /<div style="padding:0px 5px 0px 5px;">.*?<a href='(.*?)' style="font-size:13px;">(.*?)<\/a>/gms;
         let m;
         while ((m = regex.exec(raw)) !== null) {
             if (m.index === regex.lastIndex) regex.lastIndex += 1;
