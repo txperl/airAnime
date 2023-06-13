be987d04bdbc42ec9e5f70edfa4d17a52b479527
txperl <txperl@gmail.com>
2023-06-13 21:14:30 +0800

    docs: modify readme

--- vapp/blocks/aboutArea.mjs
+++ vapp/blocks/aboutArea.mjs
@@ -4,7 +4,7 @@ const template = `
         <h1>About</h1>
         <p>airAnime 是一款聚合「番剧搜索」工具，也许你会喜欢。</p>
         <a href="https://yumoe.com" target="_blank" class="mdui-chip">
-            <span class="mdui-chip-title">Version@v3.08 / Created by Trii Hsia with ❤️</span>
+            <span class="mdui-chip-title">Version@v3.09 / Created by Trii Hsia with ❤️</span>
         </a>
         <a href="mailto:txperl@gmail.com" target="_blank" class="mdui-chip">
             <span class="mdui-chip-title">#Email</span>
